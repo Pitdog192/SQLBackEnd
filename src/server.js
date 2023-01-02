28c36db3ce2ca68dcfc45ad24958c73198fccb31
santiago <arroqui192@gmail.com>
2023-01-02 15:50:48 -0300

    servidor proxy con cluster y fork, probar cluster

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import  express from 'express';
+import express from 'express';
 import session from 'express-session';
 import routerProductos from './Router/routerProductos.js';
 import routerCarrito from './Router/routerCarrito.js';
@@ -22,49 +22,73 @@ const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
 const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true}
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
-app.use(express.static('public'));
-app.use(cookieParser('ecommerce'))
-app.use(session({
-    store: MongoStore.create({
-        mongoUrl: config.mongodb.url,
-        collectionName: 'sessions',
-        mongoOptions: advancedOptions,
-        ttl: 600
-    }),
-    secret: 'ecommerce',
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use('/productos', checkAuth, routerProductos); //RUTA DE PRODUCTOS
-app.use('/carrito', routerCarrito); // RUTA DE CARRITO
-app.use('/', routerLogin); //RUTA LOGIN
-app.use('/info', routerProcess)// RUTA YARGS
-app.use('/api', routerRandom);//RUTA RANDOM CON FORKS
-app.set('port', args.port);
-//EJS
-app.set('views', './views/ejs')
-app.set('view engine', 'ejs');
-//CONECCION A MONGODBATLAS
-try {
-    await mongoose.connect(config.mongodb.url);
-} catch (error) {
-    console.log(error)
-} 
 
-// SOCKETS-------------------------------
-socketIo(io);
-// FIN DE SOCKETS-------------------------
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
-//RESPUESTA PARA RUTAS NO IMPLEMENTADAS
-app.use('*', (req, res) => {
-    let path = req.params;
-    res.send({ Error_ruta: `La ruta: '${path[0]}' no está implementada` });
-});
+const port = args.port
+const MODO = args.modo
 
-app.set('port', args.port);
-const server = httpServer.listen(app.get('port'), () => console.log(`Server escuchando, http://localhost:${app.get('port')}/login`));
-server.on('error', (error) => console.log(`Error: ${error}`));
+if (MODO == 'cluster' && cluster.isPrimary) {
+    console.log(`Numero de procesadores ${numCPUs}`)
+    console.log(`PID Master ${process.pid}`)
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+        cluster.fork()
+    })
+} else {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended:true }));
+    // app.use(express.static('public'));
+    app.use(cookieParser('ecommerce'))
+    app.use(session({
+        store: MongoStore.create({
+            mongoUrl: config.mongodb.url,
+            collectionName: 'sessions',
+            mongoOptions: advancedOptions,
+            ttl: 600
+        }),
+        secret: 'ecommerce',
+        resave: false,
+        saveUninitialized: false
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use('/productos', checkAuth, routerProductos); //RUTA DE PRODUCTOS
+    app.use('/carrito', routerCarrito); // RUTA DE CARRITO
+    app.use('/', routerLogin); //RUTA LOGIN
+    app.use('/info', routerProcess)// RUTA YARGS
+    app.use('/api', routerRandom);//RUTA RANDOM CON FORKS
+    app.set('port', args.port);
+    //EJS
+    app.set('views', './views/ejs')
+    app.set('view engine', 'ejs');
+    //CONECCION A MONGODBATLAS
+    try {
+        await mongoose.connect(config.mongodb.url);
+    } catch (error) {
+        console.log(error)
+    } 
+    
+    // SOCKETS-------------------------------
+    socketIo(io);
+    // FIN DE SOCKETS-------------------------
+    
+    app.get('/', (req,res) => {
+        console.log(`port: ${port} -> Fyh: ${Date.now()}`)
+        res.send(`Servidor express <span style="color:blueviolet;">(PM2)</span> en ${port} - <b>PID ${process.pid}</b> - ${new Date().toLocaleString()}`)
+    })
+    //RESPUESTA PARA RUTAS NO IMPLEMENTADAS
+    app.use('*', (req, res) => {
+        let path = req.params;
+        res.send({ Error_ruta: `La ruta: '${path[0]}' no está implementada` });
+    });
+    
+    
+    const server = httpServer.listen(port, () => console.log(`Server escuchando, http://localhost:${port}/login`));
+    server.on('error', (error) => console.log(`Error: ${error}`));
+}
--- yargs.js
+++ yargs.js
@@ -2,10 +2,12 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const yargs = require('yargs/yargs')(process.argv.slice(2))
 const args = yargs.alias({
-        p: 'port'
+        p: 'port',
+        m: 'modo'
     })
     .default({
-        port: 8080
+        port: 8080,
+        modo:'fork'
     })
     .argv
 
